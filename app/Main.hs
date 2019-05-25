84793cec13e43cc81be346987237423d12caa43a
YinYin Chiu <cychiuae@connect.ust.hk>
2019-05-25 12:26:12 +0800

    Implement print words function

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,11 @@ module Main where
 
 import Lib
 
+printSomeWords :: String -> IO ()
+printSomeWords words = putStrLn words
+
 helloWorld :: IO ()
-helloWorld = putStrLn "Hello World"
+helloWorld = printSomeWords "Hello World"
 
 main :: IO ()
 main = helloWorld
